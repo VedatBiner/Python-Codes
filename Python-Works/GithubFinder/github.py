b7b9207c421c7d861309f28a6bbd90ea2dbfa4cc
Vedat Biner <vbiner@gmail.com>
2022-12-20 10:01:32 +0300

    GithubFinder

---
+++ Python-Works/GithubFinder/github.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import requests
+app = Flask(__name__)
+base_url = "https://api.github.com/users/"
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST": # POST ise
+        # formdan githubname alıyoruz
+        githubname = request.form.get("githubname")
+        # kullanıcı bilgileri
+        response_user = requests.get(base_url + githubname)
+        user_info =response_user.json()
+        # repo bilgileri alalım
+        response_repos = requests.get(base_url + githubname + "/repos")
+        repos = response_repos.json()
+
+        if "message" in user_info:
+            return render_template("index.html", error="Kullanıcı bulunamadı...")
+        else:
+            return render_template("index.html", profile = user_info, repos = repos)
+    else:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug = True)
